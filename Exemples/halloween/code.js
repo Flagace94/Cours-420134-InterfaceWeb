8119ced601028972a17f99837109aa1de2c38b34
Martin C <martin.couture@uqtr.ca>
2020-10-20 15:18:00 -0400

    Ajout exemple

---
+++ Exemples/halloween/code.js
@@ -0,0 +1,24 @@
+function decompte() {
+    var j = new Date();
+    var halloween = new Date(2020, 9, 31);
+    var nbJours = Math.round((halloween- j )/1000/60/60/24);
+    
+    document.getElementById("jours").innerHTML = nbJours;
+}
+
+function animationIN(){
+    nbElement = document.getElementById("decompte").getElementsByClassName("texteBoite").length
+    for(i=0;i<nbElement;i++){
+        document.getElementById("decompte").getElementsByClassName("texteBoite")[i].style.display = "none";
+    }
+    document.getElementById("bat").style.display = "block"; 
+}
+
+function animationOUT(){
+    nbElement = document.getElementById("decompte").getElementsByClassName("texteBoite").length
+    for(i=0;i<nbElement;i++){
+        document.getElementById("decompte").getElementsByClassName("texteBoite")[i].style.display = "block";
+    }
+
+    document.getElementById("bat").style.display = "none"; 
+}
